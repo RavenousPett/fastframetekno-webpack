699611e66b58c879d0e75a202aeb9a65d243e47d
Richard Pett <rich_pett@hotmail.com>
2016-07-11 14:59:41 +0100

    Stylesheet not being loaded :(

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
 // Require bootstrap 3 http://bline.github.io/bootstrap-webpack-example
 require('bootstrap-loader');
 
+// Custom styles
+require('../css/style.scss');
+
 var $ = require('jquery');
 $('#target').html('hello the world!');
-require('../css/style.scss');
