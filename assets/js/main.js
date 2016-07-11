378033a6770157315ee9c5e3949896108bb62616
Richard Pett <rich_pett@hotmail.com>
2016-07-11 15:20:48 +0100

    Load bootstrap before customs styles :)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,8 @@
-// Custom styles
-require('../css/style.scss');
-
 // Require bootstrap 3 http://bline.github.io/bootstrap-webpack-example
 require('bootstrap-loader');
+
+// Custom styles
+require('../css/style.scss');
 
 var $ = require('jquery');
 $('#target').html('hello the world!');
